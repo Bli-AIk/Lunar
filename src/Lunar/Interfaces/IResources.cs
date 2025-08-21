1488d05d4523665399ca0a6378d589aa11e2e1eb
Bli_Aik <haikun2333@126.com>
2025-08-21 22:47:08 +0800

    feat(Interfaces/IResources): Add LoadAll methods with IEnumerable<string> paths and async variants

--- src/Lunar/Interfaces/IResources.cs
+++ src/Lunar/Interfaces/IResources.cs
@@ -26,6 +26,14 @@ namespace Lunar.Interfaces
         IEnumerable<T> LoadAll<T>(string path);
 
         /// <summary>
+        ///     Loading resources from folder path
+        /// </summary>
+        /// <param name="paths">The paths to the resources</param>
+        /// <typeparam name="T">Resource type</typeparam>
+        /// <returns>Resources</returns>
+        IEnumerable<T> LoadAll<T>(IEnumerable<string> paths);
+
+        /// <summary>
         ///     Release resource.
         /// </summary>
         /// <param name="resource">Resource</param>
@@ -48,5 +56,23 @@ namespace Lunar.Interfaces
         /// <typeparam name="T">Resource type</typeparam>
         /// <returns>Resource</returns>
         Task<T> LoadAsync<T>(string path, CancellationToken ct = default);
+
+        /// <summary>
+        ///     (Async) Loading resources from folder path
+        /// </summary>
+        /// <param name="path">The path to the folder</param>
+        /// <param name="ct">CancellationToken</param>
+        /// <typeparam name="T">Resource type</typeparam>
+        /// <returns>Resources</returns>
+        Task<IEnumerable<T>> LoadAllAsync<T>(string path, CancellationToken ct = default);
+
+        /// <summary>
+        ///     (Async) Loading resources from folder path
+        /// </summary>
+        /// <param name="paths">The paths to the resources</param>
+        /// <param name="ct">CancellationToken</param>
+        /// <typeparam name="T">Resource type</typeparam>
+        /// <returns>Resources</returns>
+        Task<IEnumerable<T>> LoadAllAsync<T>(IEnumerable<string> paths, CancellationToken ct = default);
     }
 }
