5c33e107d8a528862c7206a691e5d5067e5f55bd
Bli_Aik <haikun2333@126.com>
2025-08-27 13:01:23 +0800

    feat(logging): add ILogger interface with basic logging methods

---
+++ src/Lunar/Interfaces/ILogger.cs
@@ -0,0 +1,9 @@
+namespace Lunar.Interfaces
+{
+    public interface ILogger
+    {
+        void Log(object message, object? context = null);
+        void LogWarning(object message, object? context = null);
+        void LogError(object message, object? context = null);
+    }
+}
