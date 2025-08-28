ebae188268e67b11e89bcb1ffc18738516307b58
Bli_Aik <haikun2333@126.com>
2025-08-28 13:29:06 +0800

    chore(Components): Rename ViewComponent to GameObjectComponent and add constructors
    - Rename ViewComponent.cs to GameObjectComponent.cs
    - Add constructor to GameObjectComponent struct
    - Add constructors to PositionComponent struct
    - Remove obsolete IGameObject interface

--- src/Lunar/Components/GameObjectComponent.cs
+++ src/Lunar/Components/GameObjectComponent.cs
@@ -2,6 +2,11 @@ namespace Lunar.Components
 {
     public struct GameObjectComponent
     {
+        public GameObjectComponent(GameObject gameObject)
+        {
+            GameObject = gameObject;
+        }
+
         public GameObject GameObject;
     }
 }
--- src/Lunar/Components/PositionComponent.cs
+++ src/Lunar/Components/PositionComponent.cs
@@ -2,6 +2,20 @@ namespace Lunar.Components
 {
     public struct PositionComponent
     {
+        public PositionComponent(float x, float y)
+        {
+            X = x;
+            Y = y;
+            Z = 0;
+        }
+
+        public PositionComponent(float x, float y, float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
         public float X, Y, Z;
     }
 }
