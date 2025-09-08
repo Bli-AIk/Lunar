0ab13c16131d28513bf082a9abe83e3f365b0644
Bli_Aik <haikun2333@126.com>
2025-09-08 18:51:00 +0800

    Remove Lunar.Core.Base from parent repo (preparing submodule)

--- src/Lunar.Core.Base/Interfaces/ILogger.cs
+++
@@ -1,8 +0,0 @@
-namespace Lunar.Core.Base.Interfaces;
-
-public interface ILogger
-{
-    void Log(object message, object? context = null);
-    void LogWarning(object message, object? context = null);
-    void LogError(object message, object? context = null);
-}
